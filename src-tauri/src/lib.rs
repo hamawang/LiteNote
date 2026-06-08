8ae6db31de243cd2292cf4b6c5d4444cb91df9b0
SeaZhusp <32945598+SeaZhusp@users.noreply.github.com>
2026-06-08 12:41:42 +0800

    记住窗口位置

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -373,6 +373,7 @@ pub fn run() {
         .plugin(tauri_plugin_global_shortcut::Builder::new().build())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_window_state::Builder::default().build())
         .plugin(tauri_plugin_autostart::init(
             tauri_plugin_autostart::MacosLauncher::LaunchAgent,
             None,
